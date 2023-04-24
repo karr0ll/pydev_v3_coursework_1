0ac0ba097b3ddf48c628654c13df9fd406274e9d
Kirill Tuchkin <volkirtu@gmail.com>
2023-04-24 18:38:55 +0300

    minor fixes

--- app/utils.py
+++ app/utils.py
@@ -1,6 +1,6 @@
 import json
 import operator
-
+import os.path
 
 
 def load_json_sorted():
@@ -9,11 +9,11 @@ def load_json_sorted():
     сортирует словари по убыванию даты, фильтрует транзакции по значению "EXECUTED"
     :return: отсортированный по убыванию даты список словарей с данными
     """
-    with open("../operations.json", "r", encoding="UTF-8") as file:
+    filedir = os.path.dirname(os.path.abspath(__file__))
+    with open(os.path.join(filedir[:-3], "operations.json"), "r", encoding="UTF-8") as file:
         data = json.load(file)
         filtered_data = filter(None, data)
         sorted_data = sorted(filtered_data, key=operator.itemgetter('date'), reverse=True)
         executed_transactions = [d for d in sorted_data if d["state"] == "EXECUTED"]
         return executed_transactions
-
 
